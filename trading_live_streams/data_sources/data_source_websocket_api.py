8e927bc3d3fc7bc86a168c31dab09f6cb8798c09
Shivani Parate <shivanidutt602@gmail.com>
2026-04-02 03:18:07 +0530

    modified docker port to 9000

--- trading_live_streams/data_sources/data_source_websocket_api.py
+++ trading_live_streams/data_sources/data_source_websocket_api.py
@@ -14,31 +14,32 @@ from setup_connection.source_url_connections import setup_websocket_connection
 
 init_logging()
 logger = logging.getLogger(__name__)
-    # Websocket Exchanges
+# Websocket Exchanges
 
 exchanges = {
-        "binance": "wss://ws-api.binance.com:443/ws-api/v3"
-                    # "coinbase": "wss://ws-feed.exchange.coinbase.com"
-        }
-        # === Add more exchanges and their websocket URLs here
+    "binance": "wss://ws-api.binance.com:443/ws-api/v3"
+    # "coinbase": "wss://ws-feed.exchange.coinbase.com"
+}
+# === Add more exchanges and their websocket URLs here
+
 
 # Create a single kafka producer to be shared by all the exchanges
 async def create_kafka_producer():
 
     producer = aiokafka.AIOKafkaProducer(
-        bootstrap_servers='localhost:9092',
+        bootstrap_servers="localhost:9092",
         client_id="crypto-pipeline-shared-producer",
         value_serializer=lambda v: json.dumps(v).encode("utf-8"),
-        linger_ms=10
-
-        #=== add more data compression techniques here
+        linger_ms=10,
+        # === add more data compression techniques here
     )
     await producer.start()
     logger.info("Kafka Producer started")
     return producer
 
+
 # function to handle all the keyboard interrupt
-async def shutdown(signal , loop, producer , tasks):
+async def shutdown(signal, loop, producer, tasks):
     # to stop the producer when keyboard interrupt happens
 
     logger.info("Recieved Stop signal (Keyboard Interrupt)")
@@ -56,11 +57,14 @@ async def main():
     # create one websocket task per exchange
     tasks = []
     for source_name, source_url in exchanges.items():
-        task = asyncio.create_task(setup_websocket_connection(
-            source_name= source_name, 
-            source_websocket_url= source_url, 
-            producer= producer))
-        
+        task = asyncio.create_task(
+            setup_websocket_connection(
+                source_name=source_name,
+                source_websocket_url=source_url,
+                producer=producer,
+            )
+        )
+
         tasks.append(task)
     logger.info("All tasks created for all exchange url")
 
@@ -73,12 +77,11 @@ async def main():
     try:
         await asyncio.gather(*tasks)
     except asyncio.CancelledError:
-        pass #===  in case of shutdown close the producer also
+        pass  # ===  in case of shutdown close the producer also
     finally:
         await producer.stop()
         logger.info("Producer Stopped.")
 
+
 if __name__ == "__main__":
     asyncio.run(main())
-
-
